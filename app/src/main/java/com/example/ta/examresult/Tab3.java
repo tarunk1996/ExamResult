79de798c129656ef22fe8efed37f7aad612d7b86
Tarun <tarunk.1996@gmail.com>
2017-04-27 07:41:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ta/examresult/Tab3.java
@@ -0,0 +1,20 @@
+package com.example.ta.examresult;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by TA on 27-04-2017.
+ */
+
+public class Tab3 extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        return rootView;
+    }
+}
